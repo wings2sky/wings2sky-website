027fc7b1462d24183a4524d3c0d2ed0d3350f8a4
prince rajput <prince.rajput@sveltetech.com>
2025-10-28 18:04:27 +0530

    fixed github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // required for static export
+  images: { unoptimized: true }, // required because GitHub Pages doesn't support Image Optimization
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-}
+  basePath: '/wings2sky-website', // 👈 important for GitHub Pages
+  assetPrefix: '/wings2sky-website/', // 👈 ensures all static assets load correctly
+};
 
-export default nextConfig
+export default nextConfig;
