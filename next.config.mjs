dfd1052e3846bfc30632e1d4718b613292bdfbb7
wings2sky <Connect@wings2sky.com>
2025-11-22 13:14:28 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,42 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
 
-export default nextConfig
+  // ⭐ Add all redirects here
+  async redirects() {
+    return [
+      {
+        source: "/blogs/1",
+        destination: "/blogs/amazon-a-plus-content-guide",
+        permanent: true,
+      },
+      {
+        source: "/blogs/2",
+        destination: "/blogs/sales-performance-analytics",
+        permanent: true,
+      },
+      {
+        source: "/blogs/3",
+        destination: "/blogs/easy-category-approval-guide",
+        permanent: true,
+      },
+      {
+        source: "/blogs/4",
+        destination: "/blogs/mobile-ecommerce-conversion-strategies",
+        permanent: true,
+      },
+      {
+        source: "/blogs/5",
+        destination: "/blogs/seller-account-setup-amazon-filpkart",
+        permanent: true,
+      },
+      {
+        source: "/blogs/6",
+        destination: "/blogs/5-minute-product-listing-audit",
+        permanent: true,
+      }
+    ];
+  },
+};
+
+export default nextConfig;
